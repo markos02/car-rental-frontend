6b7da067b9c9b8753e8e40bcfb48d99173474014
mk <kosakowski.m@gmail.com>
2023-04-12 19:21:48 +0200

    First Commit

---
+++ src/test/java/com/kodilla/carrentalfrontend/CarRentalFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.carrentalfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
